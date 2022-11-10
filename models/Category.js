5458443af39157464644575aca11bbfd1e590725
damiandlg <damian.delagarzaf@gmail.com>
2022-11-09 19:22:45 -0600

    database models completed with necessary information

--- models/Category.js
+++ models/Category.js
@@ -6,7 +6,16 @@ class Category extends Model {}
 
 Category.init(
   {
-    // define columns
+    //deine id column
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      autoIncrement: true
+    },
+    category_name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
   },
   {
     sequelize,
